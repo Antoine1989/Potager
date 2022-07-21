2ccf25eedf3320eb332ad9bef49389d092a1273a
Antoine1989 <antoine-guillou@orange.fr>
2022-07-21 11:06:48 +0200

    initialisation

---
+++ src/main/java/fr/formation/gestionPotager/GestionPotagerApplication.java
@@ -0,0 +1,13 @@
+package fr.formation.gestionPotager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionPotagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionPotagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/formation/gestionPotager/GestionPotagerApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.formation.gestionPotager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionPotagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
